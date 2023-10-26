537f50d51f252d3e84cf20dedd29a38fa851f6d3
Juan Manuel Gimeno Illa <jmgimeno@gmail.com>
2023-10-26 09:29:18 +0200

    A simple example using h2 and jdbc

---
+++ jdbc-h2/src/main/java/cat/udl/eps/amp/jdbch2/datasource/DumpBooks.java
@@ -0,0 +1,24 @@
+package cat.udl.eps.amp.jdbch2.datasource;
+
+import cat.udl.eps.amp.jdbch2.datasource.db.BookDAO;
+import cat.udl.eps.amp.jdbch2.datasource.domain.Book;
+import org.h2.jdbcx.JdbcConnectionPool;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class DumpBooks {
+
+    static DataSource dataSource = JdbcConnectionPool.create(
+            "jdbc:h2:mem:test;INIT=RUNSCRIPT FROM 'jdbc-h2/scripts/init.sql'", "sa", "sa");
+
+    public static void main(String[] args) {
+        try {
+            var bookDAO = new BookDAO(dataSource);
+            List<Book> books = bookDAO.allBoks();
+            System.out.println(books);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ jdbc-h2/src/main/java/cat/udl/eps/amp/jdbch2/datasource/domain/Book.java
@@ -0,0 +1,52 @@
+package cat.udl.eps.amp.jdbch2.datasource.domain;
+
+public class Book {
+    private long id;
+    private String title;
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(long id, String title, String author) {
+        setId(id);
+        setTitle(title);
+        setAuthor(author);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if (title == null) throw new IllegalArgumentException("title cannot be null");
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        if (author == null) throw new IllegalArgumentException("author cannot be null");
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title=" + title +
+                ", author=" + author +
+                '}';
+    }
+
+}
