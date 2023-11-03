7f07f5447546bef3f3f51b71da9aab4b3efa8554
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2023-11-03 16:24:06 +0100

    Now it works on my MBP

--- jdbc-h2/build.gradle.kts
+++ jdbc-h2/build.gradle.kts
@@ -2,17 +2,11 @@ plugins {
     application
 }
 
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
-}
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.h2database:h2:2.2.220")
+    implementation("com.h2database:h2:2.2.224")
     implementation("org.flywaydb:flyway-core:9.22.3")
 }
