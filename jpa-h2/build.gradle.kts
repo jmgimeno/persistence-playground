24f38320d8f562fca042e82515bb0d14cb5c66e9
Juan Manuel Gimeno Illa <jmgimeno@diei.udl.cat>
2023-11-07 12:09:24 +0100

    Changed h2 version to satisfy hibernate

--- jpa-h2/build.gradle.kts
+++ jpa-h2/build.gradle.kts
@@ -16,7 +16,7 @@ idea {
 dependencies {
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
     implementation("org.hibernate:hibernate-core:6.3.1.Final")
-    implementation("com.h2database:h2:2.1.210")
+    implementation("com.h2database:h2:2.1.214")
     implementation("org.flywaydb:flyway-core:10.0.0")
     implementation("org.slf4j:slf4j-api:2.0.9")
     runtimeOnly("org.slf4j:slf4j-simple:2.0.9")
